fe9262029cc863bb524a65d4988c11e079c52832
andrei_kozel <andrei.kozel@p2p.org>
2025-05-21 12:37:34 +0300

    LAM-1447 Reworked portfolio service

--- webapp/src/services/portfolio.ts
+++ webapp/src/services/portfolio.ts
@@ -1,31 +1,48 @@
 import {
+  Cache,
   fetchersByAddressSystem,
   getCache,
   runFetchersByNetworkId,
 } from '@sonarwatch/portfolio-plugins';
 import { logger } from '../logger/logger';
 
-const cache = getCache();
+class PortfolioService {
+  private static instance: PortfolioService;
 
-const getPortfolio = async (address: string) => {
-  const fetchers = fetchersByAddressSystem['solana'];
-  const result = await runFetchersByNetworkId(
-    address,
-    'solana',
-    fetchers,
-    cache,
-  );
-  const addresses: string[] = []
-  result.elements.forEach(element => {
-    if (element.type === 'multiple') {
-      element.data.assets
-        .forEach(a => a.data.address && addresses.push(a.data.address));
+  private readonly cache: Cache;
+
+  private constructor() {
+    this.cache = getCache();
+  }
+
+  public static getInstance(): PortfolioService {
+    if (!PortfolioService.instance) {
+      PortfolioService.instance = new PortfolioService();
     }
-  })
-  logger.info(addresses, 'Tokens collected.');
-  const tokenInfo = await cache.getTokenPrices(addresses, 'solana');
-  logger.info(tokenInfo, 'Tokens are laded');
-  return { ...result, tokenInfo };
-};
+    return PortfolioService.instance;
+  }
 
-export { getPortfolio };
+  public getPortfolio = async (address: string) => {
+    const fetchers = fetchersByAddressSystem['solana'];
+    const result = await runFetchersByNetworkId(
+      address,
+      'solana',
+      fetchers,
+      this.cache
+    );
+    const addresses: string[] = [];
+    result.elements.forEach((element) => {
+      if (element.type === 'multiple') {
+        element.data.assets.forEach(
+          (a) => a.data.address && addresses.push(a.data.address)
+        );
+      }
+    });
+    logger.info(addresses, 'Tokens collected.');
+    const tokenInfo = await this.cache.getTokenPrices(addresses, 'solana');
+    logger.info(tokenInfo, 'Tokens are laded');
+    return { ...result, tokenInfo };
+  };
+}
+
+export default PortfolioService.getInstance();
